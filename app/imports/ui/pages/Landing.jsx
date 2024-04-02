8cdb882828933bbdbc92aed340233fea5b54d887
Andrew Gibbons <156730995+AndrewGibbons575@users.noreply.github.com>
2024-04-01 21:40:38 -1000

    Frustration

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => (
   <Container id="landing-page" className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <PeopleFill size={100}/>
+        <PeopleFill size={100} />
         <h1>Multiple Users</h1>
         <h5>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h5>
       </Col>
